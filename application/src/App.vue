1096ee7cce2006ff8ae15f98510dcab0fa68a074
Marcus Vilhelmsson <55685699+marcusvilhelmsson@users.noreply.github.com>
2021-07-30 21:35:12 +0200

    Projekt Setup ( Docker + Vue )
    +Bootstrap
    +Fontawesome

---
+++ application/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div> 
+    <Navigation/>
+    <Home/>
+  </div>
+</template>
+
+<script>
+import Navigation from './components/nav/Navigation.vue'
+import Home from './components/views/Home.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Navigation,
+    Home
+  }
+}
+</script>
+
---
+++ application/src/main.js
@@ -0,0 +1,12 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import "../public/style.scss";
+
+library.add(fas)
+
+const app = createApp(App)
+app.component('font-awesome-icon', FontAwesomeIcon)
+app.mount('#app')
