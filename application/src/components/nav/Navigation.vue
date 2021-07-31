16d8635663fe7d4ce8a6d12819b3bfb1807def95
Marcus Vilhelmsson <55685699+marcusvilhelmsson@users.noreply.github.com>
2021-07-31 15:53:44 +0200

    Nav styling

--- application/src/components/nav/Navigation.vue
+++ application/src/components/nav/Navigation.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="bg-dark" id="nav">
     <div class="collapse container" id="navbarToggleExternalContent">
-        <div class="row ">
+        <div class="row">
             <div class="col-sm-5">
               <h4 class="text-white">About</h4>
               <p class="text-muted">Add some information about the album below, the author, or any other background context. Make it a few sentences long so folks can pick up some informative tidbits. Then, link them off to some social networking sites or contact information.</p>
